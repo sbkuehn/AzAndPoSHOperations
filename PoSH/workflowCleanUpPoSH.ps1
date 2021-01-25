fbdb215d161f152a466bd3c6ba70bfc0dd1438fc
Shannon Kuehn <shannon.kuehn@gmail.com>
2021-01-25 15:42:35 -0600

    Update workflowCleanUpPoSH.ps1

--- PoSH/workflowCleanUpPoSH.ps1
+++ PoSH/workflowCleanUpPoSH.ps1
@@ -1,7 +1,7 @@
 workflow Uninstall-AzureModules
 {
-    $Modules = (Get-Module -ListAvailable Azure*).Name |Get-Unique
-    Foreach -parallel ($Module in $Modules)
+    $Modules = (Get-Module -ListAvailable Az*).Name |Get-Unique
+    ForEach -parallel ($Module in $Modules)
     { 
         Write-Output ("Uninstalling: $Module")
         Uninstall-Module $Module -Force
