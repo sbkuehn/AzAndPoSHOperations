7a692bde8a2193c46a5fe2370ee3c4ab1307c7db
Shannon Kuehn <shannon.kuehn@gmail.com>
2021-03-13 15:06:50 -0600

    Create logAnalyticsInventory.ps1

---
+++ PoSH/logAnalyticsInventory.ps1
@@ -0,0 +1,11 @@
+$results = @()
+$workspaces = Search-AzGraph -Query "where type =~ 'Microsoft.OperationalInsights/workspaces'"
+ForEach($workspace in $workspaces){ 
+    $obj = New-Object -TypeName PSObject
+    $obj | Add-Member -MemberType NoteProperty -Name name -Value $workspace.name 
+    $obj | Add-Member -MemberType NoteProperty -Name location -Value $workspace.location 
+    $obj | Add-Member -MemberType NoteProperty -Name resourceGroup -Value $workspace.resourceGroup
+    $obj | Add-Member -MemberType NoteProperty -Name sku -Value $workspace.properties.sku 
+    $results += $obj
+}
+$results | Export-Csv C:\error.csv -NoTypeInformation
