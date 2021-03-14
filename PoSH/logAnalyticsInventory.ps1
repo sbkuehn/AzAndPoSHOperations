2d0732289ba08be4a49a10931475af23b5c20ee3
Shannon Kuehn <shannon.kuehn@gmail.com>
2021-03-14 18:02:12 -0500

    Update logAnalyticsInventory.ps1

--- PoSH/logAnalyticsInventory.ps1
+++ PoSH/logAnalyticsInventory.ps1
@@ -1,3 +1,19 @@
+<#
+Created 
+2021.03.14
+Shannon Kuehn
+Last Updated
+© 2021 Microsoft Corporation. 
+All rights reserved. Sample scripts/code provided herein are not supported under any Microsoft standard support program or 
+service. The sample scripts/code are provided AS IS without warranty of any kind. Microsoft disclaims all implied warranties 
+including, without limitation, any implied warranties of merchantability or of fitness for a particular purpose. The entire 
+risk arising out of the use or performance of the sample scripts and documentation remains with you. In no event shall 
+Microsoft, its authors, or anyone else involved in the creation, production, or delivery of the scripts be liable for any 
+damages whatsoever (including, without limitation, damages for loss of business profits, business interruption, loss of 
+business information, or other pecuniary loss) arising out of the use of or inability to use the sample scripts or 
+documentation, even if Microsoft has been advised of the possibility of such damages.
+#>
+
 # Exploratory queries
 # Search Azure Resource Graph for all workspaces
 Search-AzGraph -Query "where type =~ 'Microsoft.OperationalInsights/workspaces'"
