30d2b52a64ca9edb819a5feb0b30fa3539190606
Shannon Kuehn <shannon.kuehn@gmail.com>
2021-03-14 16:58:39 -0500

    Update logAnalyticsInventory.ps1

--- PoSH/logAnalyticsInventory.ps1
+++ PoSH/logAnalyticsInventory.ps1
@@ -6,15 +6,20 @@ Search-AzGraph -Query "where type =~ 'Microsoft.OperationalInsights/workspaces'"
 $workspaces = Search-AzGraph -Query "where type =~ 'Microsoft.OperationalInsights/workspaces'"
 $workspaces
 
-# Run these queries to ensure you gather all the right fields to form the inventory report. 
+# Run these cmdlets to ensure you gather all the right fields to form the inventory report. 
 # You will want the name, location, resource group, SKU, and features to ensure your Log Analytics workspace is designed correctly to meet the needs of the enterprise.
 $workspaces.name
 $workspaces.location
 $workspaces.resourceGroup
+
+# Run the next cmdlet to see the SKU and permissions model information:
+$workspaces.properties
+
+# From there, you should be able to extract the right information by running the next two cmdlets:
 $workspaces.properties.sku
 $workspaces.properties.features
 
-# This script creates an inventory report of all Log Analytics workspaces. I
+# This script creates an inventory report of all Log Analytics workspaces and extracts the name, location, resource group, SKU, and permissions model into a CSV file.
 $results = @()
 $workspaces = Search-AzGraph -Query "where type =~ 'Microsoft.OperationalInsights/workspaces'"
 ForEach($workspace in $workspaces){ 
