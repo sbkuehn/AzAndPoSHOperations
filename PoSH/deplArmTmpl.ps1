47fac8c32794d3e8b1b3dc8fd49884ef5607bb0e
Shannon Kuehn <shannon.kuehn@gmail.com>
2021-07-05 09:22:37 -0500

    Create deplArmTmpl.ps1

---
+++ PoSH/deplArmTmpl.ps1
@@ -0,0 +1,42 @@
+[CmdletBinding()]
+    param(
+    [Parameter(Mandatory=$true)]
+    [string]$location,
+    [Parameter(Mandatory=$true)]
+    [string]$rgName,
+    [Parameter(Mandatory=$true)]
+    [string]$resDeplName,
+    [Parameter(Mandatory=$true)]
+    [string]$tmplPath,
+    [Parameter(Mandatory=$true)]
+    [string]$tmplFile,
+    [Parameter(Mandatory=$true)]
+    [string]$tmpl,
+    [Parameter(Mandatory=$true)]
+    [string]$paramPath,
+    [Parameter(Mandatory=$true)]
+    [string]$paramFile,
+    [Parameter(Mandatory=$true)]
+    [string]$params
+    )
+    
+New-AzResourceGroup `
+    -Name $rgName `
+    -Location $location `
+    -Verbose -Force
+
+New-AzResourceGroupDeployment `
+    -Name $resDeplName `
+    -ResourceGroupName $rgName `
+    -TemplateFile $templ `
+    -TemplateParameterFile $params
+
+# Submit template as a job via PowerShell:
+$job = New-AzResourceGroupDeployment `
+    -Name $resDeplName `
+    -ResourceGroupName $rgName `
+    -TemplateFile $templ `
+    -TemplateParameterFile $params -AsJob
+
+# Check status of the job
+$job.StatusMessage
