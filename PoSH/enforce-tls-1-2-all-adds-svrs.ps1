c2ea1338996f86ebadf8c08719e4c3d0555bfa00
Shannon Kuehn <shannon.kuehn@gmail.com>
2021-09-02 14:39:32 -0500

    Update enforce-tls-1-2-all-adds-svrs.ps1

--- PoSH/enforce-tls-1-2-all-adds-svrs.ps1
+++ PoSH/enforce-tls-1-2-all-adds-svrs.ps1
@@ -12,5 +12,5 @@ ForEach($server in $servers){
       New-Item 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.2\Client' -Force | Out-Null
       New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.2\Client' -name 'Enabled' -value '1' -PropertyType 'DWord' -Force | Out-Null
       New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.2\Client' -name 'DisabledByDefault' -value 0 -PropertyType 'DWord' -Force | Out-Null
-      Write-Host 'TLS 1.2 has been enabled for $server.'
+      Write-Host 'TLS 1.2 has been enabled for:' $server
 }
