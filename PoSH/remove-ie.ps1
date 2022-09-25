bda7a2693b69b033b37439a6c6cbaf6bc9035ab5
Shannon Kuehn <shannon.kuehn@gmail.com>
2022-09-24 21:03:43 -0500

    Update remove-ie.ps1

--- PoSH/remove-ie.ps1
+++ PoSH/remove-ie.ps1
@@ -1,8 +1,8 @@
 Get-WindowsOptionalFeature -Online -FeatureName *explorer*
 Import-Module ActiveDirectory
-$servers = Get-ADComputer -Filter *
+$servers = Get-ADComputer -Filter {OperatingSystem -like "*Windows*"}
 ForEach ($server in $servers){
-        Invoke-Command -ComputerName $server.DNSHostName -ScriptBlock {
+        Invoke-Command -ComputerName $server.Name -ScriptBlock {
         Disable-WindowsOptionalFeature -Online -FeatureName Internet-Explorer-Optional-amd64 -ErrorAction SilentlyContinue
     }
     Write-Host -ForegroundColor Green Removed Internet Explorer on $server.DNSHostName
