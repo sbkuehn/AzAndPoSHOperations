340b70da97d8f020d7c0b3523052cc7d3203ca40
Shannon Eldridge-Kuehn <shannon.kuehn@gmail.com>
2025-08-24 20:14:56 -0500

    Add CmdletBinding attribute to checkLASolutions.ps1

---
+++ PoSH/checkLASolutions.ps1
@@ -0,0 +1,18 @@
+<#
+.SYNOPSIS
+Checks for solutions attached to a Log Analytics workspace.
+
+.DESCRIPTION
+This snippet lists all Microsoft.OperationsManagement/solutions resources in a given resource group. Useful for verifying whether the SecurityInsights (Sentinel) solution is still attached (or other legacy solutions).
+
+.PARAMETER ResourceGroupName
+The resource group that contains the Log Analytics workspace.
+#>
+
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $true)]
+    [string]$ResourceGroupName
+)
+
+Get-AzResource -ResourceGroupName $ResourceGroupName | Where-Object { $_.ResourceType -eq "Microsoft.OperationsManagement/solutions" }
